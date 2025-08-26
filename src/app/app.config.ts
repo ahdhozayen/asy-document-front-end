67326e7a95777b36d83e04970ce72c9c5e1b222d
MahmoudTarek44 <Mahmoudtarekmahmoud44@gmail.com>
2025-08-26 14:10:39 +0300

    Upgrade Angular dependencies to version 20.x in package.json and package-lock.json; refactor Angular build configurations in angular.json and enhance app configuration for translation services in app.config.ts.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,15 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi, HttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-
-// Translation
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-// Create translation loader factory
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
-
+// Translation
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from '../infrastructure/http/auth.interceptor';
 
@@ -22,22 +17,14 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
-    provideAnimations(),
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        },
-        defaultLanguage: 'ar'
-      })
-    ),
-    provideAnimationsAsync(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({ prefix: '/src/assets/i18n/' }),
+      fallbackLang: 'ar',
+    }),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true
-    }
-  ]
+      multi: true,
+    },
+  ],
 };
